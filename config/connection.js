2fb50edd52c6dafe4f6e266895d485f9ed070b42
James Hughes <jameshughes2009@hotmail.com>
2024-04-17 22:35:16 -0400

    changed sequilize connection

--- config/connection.js
+++ config/connection.js
@@ -5,22 +5,22 @@ require('dotenv').config();
 
 let sequelize;
 // Check if JAWSDB_URL environment variable is set (indicating JawsDB MySQL service on Heroku)
-if (process.env.JAWSDB_URL) {
+// if (process.env.JAWSDB_URL) {
 
-    // Initialize Sequelize with JawsDB URL
+//     // Initialize Sequelize with JawsDB URL
     
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
-      port: 3306
+      port: process.env.PORT
     }
   );
-}
+// }
 
 module.exports = sequelize;
